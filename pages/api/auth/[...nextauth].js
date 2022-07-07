1f57cd4918e6c28151b35830f09b240b2b5a896b
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-07-07 11:33:18 -0700

    Auth fix attempt

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@ import NextAuth from 'next-auth'
 import TwitterProvider from 'next-auth/providers/twitter'
 
 export default NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-      TwitterProvider({
-        clientId: process.env.TWITTER_CLIENT_ID,
-        clientSecret: process.env.TWITTER_CLIENT_SECRET,
-        version: '2.0',
-      }),
-    ],
-    secret: 'D3DtD16kMjo5/BQ1aqtZMg3KT7OLCLWwrqLEo5NPOGE=',
-  })
+  // Configure one or more authentication providers
+  providers: [
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      version: '2.0',
+    }),
+  ],
+  secret: 'secret',
+})
